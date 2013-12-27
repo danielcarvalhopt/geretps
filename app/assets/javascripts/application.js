e2594933c8c99267dd7b95a691d56036f3b88c4e
André Santos <andreccdr@gmail.com>
2013-12-27 23:31:27 +0000

    Adds Light Blue Template

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,33 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+//= lib/jquery/jquery-2.0.3.min.js
+//= lib/icheck.js/jquery.icheck.js
+//= lib/sparkline/jquery.sparkline.js
+//= lib/jquery-ui-1.10.3.custom.js
+//= lib/jquery.slimscroll.js
+//= lib/nvd3/lib/d3.v2.js
+//= lib/nvd3/nv.d3.custom.js
+//= lib/nvd3/src/models/scatter.js
+//= lib/nvd3/src/models/axis.js
+//= lib/nvd3/src/models/legend.js
+//= lib/nvd3/src/models/multiBar.js
+//= lib/nvd3/src/models/multiBarChart.js
+//= lib/nvd3/src/models/line.js
+//= lib/nvd3/src/models/lineChart.js
+//= lib/nvd3/stream_layers.js
+//= lib/backbone/underscore-min.js
+//= lib/backbone/backbone-min.js
+//= lib/backbone/backbone.localStorage-min.js
+//= lib/bootstrap/transition.js
+//= lib/bootstrap/collapse.js
+//= lib/bootstrap/alert.js
+//= lib/bootstrap/tooltip.js
+//= lib/bootstrap/popover.js
+//= lib/bootstrap/button.js
+//= lib/bootstrap/tab.js
+//= lib/bootstrap/dropdown.js
+//= app.js
+//= settings.js
+//= index.js
+//= chat.js
