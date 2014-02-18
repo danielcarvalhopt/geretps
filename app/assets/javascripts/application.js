a690761121dbacc28ea292b91f8ffd46c7bcff88
André Santos <andreccdr@gmail.com>
2014-02-18 03:22:03 +0000

    Issue: application.js e application.sass

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,8 @@
+//= require jquery
+//= require jquery_ujs
+//= require modernizr.min.js
+//= jquery.slimscroll.min.js
+//= waypoints.min.js
+//= jquery.localscroll.min.js
+//= jquery.scrollTo.min.js
+//= jquery.slimscroll.min.js
--- db/migrate/20131121171609_create_subjects.rb
+++ db/migrate/20131121171609_create_subjects.rb
@@ -2,9 +2,9 @@ class CreateSubjects < ActiveRecord::Migration
   def change
     create_table :subjects do |t|
       t.string :name, null: false
-      t.string :academic_year, null: false
       t.references :course, index: true, null: false
       t.references :responsible, index: true, null: false
+      t.references :academic_year, index: true, null: false
     end
   end
 end
