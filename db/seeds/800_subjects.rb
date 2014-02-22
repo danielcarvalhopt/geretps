67ca064182bf6158211121557dd7ad22942118b0
Ricardo Branco <28.ricardobranco@gmail.com>
2014-02-22 19:03:40 +0000

    Change: Project seeds

--- db/seeds/800_subjects.rb
+++ db/seeds/800_subjects.rb
@@ -9,49 +9,49 @@ subjects = [
     },
     {
       name: "Engenharia Gramatical",
-      academic_year_id: 5, 
+      academic_year_id: 5,
       course_id: 2,
       responsible_id: 1
     },
     {
       name: "Processamento Estruturado de Documentos",
-      academic_year_id: 5, 
+      academic_year_id: 5,
       course_id: 2,
       responsible_id: 1
     },
     {
       name: "Scripting no Processamento de Linguagem Natural",
-      academic_year_id: 5, 
+      academic_year_id: 5,
       course_id: 2,
       responsible_id: 1
     },
     {
-      name: "Scripting no Processamento de Linguagem Natural",
-      academic_year_id: 5, 
+      name: "Projecto Integrado",
+      academic_year_id: 5,
       course_id: 2,
       responsible_id: 1
     },
     {
       name: "Laboratórios de Informática IV",
-      academic_year_id: 4, 
+      academic_year_id: 4,
       course_id: 1,
       responsible_id: 1
     },
     {
       name: "Laboratórios de Informática III",
-      academic_year_id: 3, 
+      academic_year_id: 3,
       course_id: 1,
       responsible_id: 1
     },
     {
       name: "Laboratórios de Informática II",
-      academic_year_id: 2, 
+      academic_year_id: 2,
       course_id: 1,
       responsible_id: 1
     },
     {
       name: "Laboratórios de Informática I",
-      academic_year_id: 1, 
+      academic_year_id: 1,
       course_id: 1,
       responsible_id: 1
     }
