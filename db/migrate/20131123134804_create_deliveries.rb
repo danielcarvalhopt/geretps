ef1516a18caed074a4f12638d08875354bc6bae5
Ricardo Branco <28.ricardobranco@gmail.com>
2014-02-23 23:57:11 +0000

    Change: grades field on deliveries table to evaluated

--- db/migrate/20131123134804_create_deliveries.rb
+++ db/migrate/20131123134804_create_deliveries.rb
@@ -3,7 +3,7 @@ class CreateDeliveries < ActiveRecord::Migration
     create_table :deliveries do |t|
       t.text :description
       t.boolean :public, default: false
-      #t.boolean :grades, default: false
+      t.boolean :evaluated, default: false
       t.references :phase, index: true, null: false
       t.references :group, index: true, null: false
       t.references :statement, index: true#, null: false
--- db/seeds/014_deliveries.rb
+++ db/seeds/014_deliveries.rb
@@ -4,24 +4,28 @@ deliveries = [
 	{
 		description: "Relatório da Primeira Fase do Projecto Integrado (GereTPs). Neste relatório é explicado todas as decisões tomadas sobre a o trabalho a faser ao longo do ano.",
 		public: true,
+		evaluated: true,
 		phase_id: 1,
 		group_id: 1
 	},
 	{
 		description: "Código e Relatório final do Projeto HASKELL - Parser JOIN, desenvolvido em Laboratórios de Informática I",
 		public: true,
+		evaluated: true,
 		phase_id: 7,
 		group_id: 4
 	},
 	{
 		description: "Código e Relatório final do Projeto HASKELL - Parser JOIN, desenvolvido em Laboratórios de Informática I",
 		public: true,
+		evaluated: true,
 		phase_id: 7,
 		group_id: 5
 	},
 	{
 		description: "Código e Relatório final do Projeto HASKELL - Parser JOIN, desenvolvido em Laboratórios de Informática I",
 		public: true,
+		evaluated: true,
 		phase_id: 7,
 		group_id: 6
 	}
