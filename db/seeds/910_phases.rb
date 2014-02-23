af815777ecaa4c5a1bebe31bfdd1036a8de6edb2
Ricardo Branco <28.ricardobranco@gmail.com>
2014-02-23 11:46:58 +0000

    Add: Phase Seeds

---
+++ db/seeds/910_phases.rb
@@ -0,0 +1,44 @@
+puts 'seeding phases...'
+
+phases = [
+	{
+		name: "Fase 1",
+		begin_date: Date.new(2013,10,13),
+      	end_date: Date.new(2013,12,9),
+      	grades: true,
+      	open: true,
+      	project_id: 2
+	},
+	{
+		name: "Fase 2",
+		begin_date: Date.new(2013,12,10),
+      	end_date: Date.new(2014,3,10),
+      	open: true,
+      	project_id: 2
+	},
+	{
+		name: "Fase 3",
+		begin_date: Date.new(2014,3,11),
+      	end_date: Date.new(2014,5,4),
+      	project_id: 2
+	},
+	{
+		name: "Fase 4",
+		begin_date: Date.new(2014,5,6),
+      	end_date: Date.new(2014,7,14),
+      	project_id: 2
+	}
+]
+
+
+phases.each do |phase|
+	Phase.where("name = ? AND project_id = ?",phase[:name],phase[:project_id]).first_or_initialize.tap do |p|
+		p.name = phase[:name]
+		p.project = Project.find(phase[:project_id])
+		p.begin_date = phase[:begin_date]
+		p.end_date = phase[:end_date]
+		p.grades = p[:grades] if p[:grades]
+		p.open = p[:open] if p[:open]
+		p.save!
+	end
+end
