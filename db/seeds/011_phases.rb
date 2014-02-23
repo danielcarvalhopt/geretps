662d29535e961ccb0289a856bb48c0a062e37ecc
Ricardo Branco <28.ricardobranco@gmail.com>
2014-02-23 13:38:08 +0000

    Change: Phases and notifications seeds

--- db/seeds/011_phases.rb
+++ db/seeds/011_phases.rb
@@ -7,39 +7,39 @@ phases = [
       	end_date: Date.new(2013,12,9),
       	grades: true,
       	open: true,
-      	project_id: 2
+      	project_id: 1
 	},
 	{
 		name: "Fase 2",
 		begin_date: Date.new(2013,12,10),
       	end_date: Date.new(2014,3,10),
       	open: true,
-      	project_id: 2
+      	project_id: 1
 	},
 	{
 		name: "Fase 3",
 		begin_date: Date.new(2014,3,11),
       	end_date: Date.new(2014,5,4),
-      	project_id: 2
+      	project_id: 1
 	},
 	{
 		name: "Fase 4",
 		begin_date: Date.new(2014,5,6),
       	end_date: Date.new(2014,7,14),
-      	project_id: 2
+      	project_id: 1
 	},
 	{
 		name: "Fase 1",
 		begin_date: Date.new(2013,11,29),
 		end_date: Date.new(2014,2,3),
-		project_id: 3,
+		project_id: 2,
 		open: true
 	},
 	{
 		name: "Fase 2",
 		begin_date: Date.new(2014,2,3),
 		end_date: Date.new(2014,7,14),
-		project_id: 3,
+		project_id: 2,
 		open: true
 	}
 ]
