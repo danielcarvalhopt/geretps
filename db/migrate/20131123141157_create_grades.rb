4ed3d154061ff604a5edd06ffae9b55abc281483
Ricardo Branco <28.ricardobranco@gmail.com>
2014-02-23 18:53:40 +0000

    Add: Groups, Deliveries and Grades seeds

--- db/migrate/20131123141157_create_grades.rb
+++ db/migrate/20131123141157_create_grades.rb
@@ -4,7 +4,7 @@ class CreateGrades < ActiveRecord::Migration
       t.float :value, precision: 5, scale: 2, null: false
       t.text :comments
       t.references :delivery, index: true, null: false
-      t.references :student, index: true, null: false
+      t.references :student, index: true#, null: false
     end
   end
 end
