6da6344d2902ed23002ddca7be967e00eeae3ac1
Ricardo Branco <28.ricardobranco@gmail.com>
2014-02-24 09:39:20 +0000

    Change: Grades seeds

--- db/seeds/015_grades.rb
+++ db/seeds/015_grades.rb
@@ -4,7 +4,20 @@ grades = [
 	{
 		value: 95,
 		comments: "Fizeram uma excelente apresentação, apenas com um defeito, excederam-se no tempo. Trabalho muito completo: introdução, estudo de mercado, funcionalidades, casos de estudo, arquitetura, workflows base especificados, modelo de dados especificado, prototipagem com mockups para as principais interfaces e modelo da homepage final, base de dados definida e criada. Apresentaram um bom planeamento e uma metodologia de trabalho e organização muito bem concebida e apoiada em sistemas eletrónicos como o github e um gestor de tarefas.",
-		delivery_id: 1
+		delivery_id: 1,
+		student_id: 1
+	},
+	{
+		value: 95,
+		comments: "Fizeram uma excelente apresentação, apenas com um defeito, excederam-se no tempo. Trabalho muito completo: introdução, estudo de mercado, funcionalidades, casos de estudo, arquitetura, workflows base especificados, modelo de dados especificado, prototipagem com mockups para as principais interfaces e modelo da homepage final, base de dados definida e criada. Apresentaram um bom planeamento e uma metodologia de trabalho e organização muito bem concebida e apoiada em sistemas eletrónicos como o github e um gestor de tarefas.",
+		delivery_id: 1,
+		student_id: 2
+	},
+	{
+		value: 95,
+		comments: "Fizeram uma excelente apresentação, apenas com um defeito, excederam-se no tempo. Trabalho muito completo: introdução, estudo de mercado, funcionalidades, casos de estudo, arquitetura, workflows base especificados, modelo de dados especificado, prototipagem com mockups para as principais interfaces e modelo da homepage final, base de dados definida e criada. Apresentaram um bom planeamento e uma metodologia de trabalho e organização muito bem concebida e apoiada em sistemas eletrónicos como o github e um gestor de tarefas.",
+		delivery_id: 1,
+		student_id: 3
 	},
 	{
 		value: 85,
