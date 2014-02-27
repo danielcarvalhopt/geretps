3ca56fd8a5f8a136bf1bc6d8b110a10e25045698
André Santos <andreccdr@gmail.com>
2014-02-27 01:34:45 +0000

    Fases de um projeto e novos widgets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def date_or_undef date
     date.try(:strftime, "%d/%m/%Y") || "Não definida"
   end
+
+  def short_date_or_undef date
+    date.try(:strftime, "%d %h %H:%M") || "Não definida"
+  end
+
+  def date_time_or_undef date
+    date.try(:strftime, "%d %h %H:%M") || "Não definida"
+  end
 end
