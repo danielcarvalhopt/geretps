44dd18cd7ac2c1c3ea6c0e552f9588ab5502d407
Ricardo Branco <28.ricardobranco@gmail.com>
2014-03-07 23:36:40 +0000

    Change: Project Grades

--- db/seeds/010_assigned_students.rb
+++ db/seeds/010_assigned_students.rb
@@ -87,6 +87,38 @@ assigned_students = [
     accepted: true
   },
   {
+    student_id: 1,
+    subject_id: 2
+  },
+  {
+    student_id: 1,
+    subject_id: 3
+  },
+  {
+    student_id: 1,
+    subject_id: 4
+  },
+  {
+    student_id: 1,
+    subject_id: 5
+  },
+  {
+    student_id: 1,
+    subject_id: 6
+  },
+  {
+    student_id: 1,
+    subject_id: 7
+  },
+  {
+    student_id: 1,
+    subject_id: 8
+  },
+  {
+    student_id: 1,
+    subject_id: 9
+  },
+  {
     student_id: 2,
     subject_id: 1,
     accepted: true
