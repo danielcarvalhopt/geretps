c70c5cc0e47dbebab8fd92049f27a27d9d97cab6
Ricardo Branco <28.ricardobranco@gmail.com>
2014-03-07 03:41:28 +0000

    Add: Project Grades

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ Geretps::Application.routes.draw do
   root to: "pages#home"
 
   get "/dashboard" => "pages#dashboard", as: :dashboard
-  get "/documents/file/:id" => "documents#show_file", as: :show_file 
+  get "/documents/file/:id" => "documents#show_file", as: :show_file
 
   post "/deliveries/add_document" => "deliveries#add_document", as: :add_document
   get "/deliveries/dowload_files_zip/:id" => "deliveries#dowload_files_zip", as: :dowload_files_zip
 
+  get "/projects/:id/grades" => "projects#grades", as: :project_grades
   get "/projects/:id/groups" => "projects#groups", as: :project_groups
+
   post "/groups/:id/add_members" => "groups#add_members", as: :add_members
 
   devise_for :users, :controllers => {:registrations => "registrations"}
