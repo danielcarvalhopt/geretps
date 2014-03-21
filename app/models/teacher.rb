e5a90c59c0a9bfa0a7471b7312c116b8259b0f71
André Santos <andreccdr@gmail.com>
2014-03-21 00:39:37 +0000

    Adiciona dashboard para um docente

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,8 +2,11 @@ class Teacher < ActiveRecord::Base
   belongs_to :user
   has_many :assigned_teachers
   has_many :subjects, through: :assigned_teachers
+  has_many :projects, through: :subjects
+  has_many :notifications, through: :projects, order: 'date desc'
+  has_many :academic_years, through: :subjects
 
   validates :user, presence: true
   
-  delegate :name, :about, :phone, :avatar, :teacher, :student?, :teacher?, :student, :teacher, :type, :email, to: :user
+  delegate :name, :about, :phone, :avatar, :teacher, :student?, :teacher?, :student, :teacher, :type, :type_pt, :first_and_last_name, :email, to: :user
 end
