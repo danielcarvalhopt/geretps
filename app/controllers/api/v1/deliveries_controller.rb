433cb71e37289e2446d8c9bf2efc082a63c0fdfd
ricardobranco <28.ricardobranco@gmail.com>
2014-07-14 10:25:29 +0100

    Avaliar grupo e aluno

--- app/controllers/api/v1/deliveries_controller.rb
+++ app/controllers/api/v1/deliveries_controller.rb
@@ -18,6 +18,39 @@ class Api::V1::DeliveriesController < Api::V1::BaseController
     respond_with(delivery)
   end
 
+  def eval_group
+    delivery = @deliveries.find(params[:id])
+    group = Group.find(params[:group])
+    value = params[:value]
+    comment = params[:comment]
+
+
+    group.students.each do |s|
+      d = Grade.where(delivery_id: delivery.id, student_id: s.id).first
+      d = Grade.new(delivery_id: delivery.id, student_id: s.id) unless d
+
+      d.value = value
+      d.comments = comment if comment
+      d.save!
+    end
+    respond_with(delivery)
+  end
+
+  def eval_student
+    delivery = @deliveries.find(params[:id])
+    student = Student.find(params[:student])
+    value = params[:value]
+    comment = params[:comment]
+
+    d = Grade.where(delivery_id: delivery.id, student_id: student.id).first
+    d = Grade.new(delivery_id: delivery.id, student_id: student.id) unless delivery
+    d.value = value
+    d.comments = comment if comment
+    d.save!
+
+    respond_with(delivery)
+  end
+
   private
 
     def set_user
